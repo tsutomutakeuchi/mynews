59ba82360e7cc8aad5c9872f92a27c0cb968b2fc
tsutomu takeuchi <disneyland.dazo23@gmail.com>
2023-07-11 14:35:07 +0000

    ユーザー認証を実装:教材の範囲

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 use App\Http\Controllers\AAAController;
