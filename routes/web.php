7f9bd1eb389ff7ddf6a0be37f744c1bf28f847af
tsutomu takeuchi <disneyland.dazo23@gmail.com>
2023-07-15 13:14:39 +0000

    PHP/Laravel 13 ニュース投稿画面を作成しよう_応用課題 #17

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,11 @@ Route::controller(NewsController::class)->group(function() {
     Route::get('XXX', 'bbb');
 });
 
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add')->middleware('auth');
-    Route::get('profile/edit', 'edit')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::get('profile/edit', 'edit')->name('profile.edit');
+    Route::post('profile/edit', 'update')->name('profile.update');
 });
 Auth::routes();
 
